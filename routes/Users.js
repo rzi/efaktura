9f509f48d5931f900f89c694397dd4004a971fc1
Rafal Zietak <rzi@vp.pl>
2020-08-16 23:22:15 +0200

    different update

--- routes/Users.js
+++ routes/Users.js
@@ -1,100 +1,105 @@
-const express = require('express')
-const users = express.Router()
-const cors = require('cors')
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const express = require("express");
+const users = express.Router();
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-//const User = require('../models/User')
-//users.use(cors())
+const User = require("../models/User");
+users.use(cors());
 
-process.env.SECRET_KEY = 'secret'
+process.env.SECRET_KEY = "secret";
 
-users.post('/register', (req, res) => {
-  const today = new Date()
+users.post("/register", (req, res) => {
+  console.log("jestem w post register");
+  const today = new Date();
   const userData = {
     first_name: req.body.first_name,
     last_name: req.body.last_name,
     email: req.body.email,
     password: req.body.password,
-    created: today
-  }
+    created: today,
+  };
 
   User.findOne({
-      where: {
-        email: req.body.email
-      }
-    })
+    where: {
+      email: req.body.email,
+    },
+  })
     //TODO bcrypt
-    .then(user => {
+    .then((user) => {
       if (!user) {
         bcrypt.hash(req.body.password, 10, (err, hash) => {
-          userData.password = hash
+          userData.password = hash;
           User.create(userData)
-            .then(user => {
+            .then((user) => {
               res.json({
-                status: user.email + 'Registered!'
-              })
+                status: user.email + "Registered!",
+              });
             })
-            .catch(err => {
-              res.send('error: ' + err)
-            })
-        })
+            .catch((err) => {
+              res.send("error: " + err);
+            });
+        });
       } else {
         res.json({
-          error: 'User already exists'
-        })
+          error: "User already exists",
+        });
       }
     })
-    .catch(err => {
-      res.send('error: ' + err)
-    })
-})
+    .catch((err) => {
+      res.send("error: " + err);
+    });
+});
 
-users.post('/login', (req, res) => {
+users.post("/login", (req, res) => {
+  console.log("jestem w login")
   User.findOne({
-      where: {
-        email: req.body.email
-      }
-    })
-    .then(user => {
+    where: {
+      email: req.body.email,
+    },
+  })
+    .then((user) => {
       if (user) {
         if (bcrypt.compareSync(req.body.password, user.password)) {
           let token = jwt.sign(user.dataValues, process.env.SECRET_KEY, {
-            expiresIn: 1440
-          })
-          res.send(token)
+            expiresIn: 1440,
+          });
+          res.send(token);
         }
       } else {
         res.status(400).json({
-          error: 'User does not exist'
-        })
+          error: "User does not exist",
+        });
       }
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(400).json({
-        error: err
-      })
-    })
-})
+        error: err,
+      });
+    });
+});
 
-users.get('/profile', (req, res) => {
-  var decoded = jwt.verify(req.headers['authorization'], process.env.SECRET_KEY)
+users.get("/profile", (req, res) => {
+  var decoded = jwt.verify(
+    req.headers["authorization"],
+    process.env.SECRET_KEY
+  );
 
   User.findOne({
-      where: {
-        id: decoded.id
-      }
-    })
-    .then(user => {
+    where: {
+      id: decoded.id,
+    },
+  })
+    .then((user) => {
       if (user) {
-        res.json(user)
+        res.json(user);
       } else {
-        res.send('User does not exist')
+        res.send("User does not exist");
       }
     })
-    .catch(err => {
-      res.send('error: ' + err)
-    })
-})
+    .catch((err) => {
+      res.send("error: " + err);
+    });
+});
 
-module.exports = users
+module.exports = users;

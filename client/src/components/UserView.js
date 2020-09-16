9e80fe866e03ee956050230266bf16362e5a2d99
Rafal Zietak <rzi@vp.pl>
2020-09-16 19:51:23 +0200

    user view crash

--- client/src/components/UserView.js
+++ client/src/components/UserView.js
@@ -1,36 +1,39 @@
-import React, { Component } from 'react'
-import jwt_decode from 'jwt-decode'
+import React, { Component } from "react";
+import jwt_decode from "jwt-decode";
 
 class UserView extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      first_name: '',
-      last_name: '',
-      email: '',
-      errors: {}
+      first_name: "",
+      last_name: "",
+      email: "",
+      errors: {},
+    };
+  }
+  componentDidMount() {
+    const token = localStorage.usertoken;
+    console.log(`token ${token}`);
+    if (localStorage.usertoken.length >0) {
+      const token = localStorage.usertoken;
+      const decoded = jwt_decode(token);
+      console.log(`decoded ${decoded}`);
+      console.log(`decoded.first_name ${decoded.first_name}`);
+      this.setState({
+        first_name: decoded.first_name,
+        last_name: decoded.last_name,
+        email: decoded.email,
+      });
     }
   }
-
-  componentDidMount() {
-    const token = localStorage.usertoken
-    const decoded = jwt_decode(token)
-    this.setState({
-      first_name: decoded.first_name,
-      last_name: decoded.last_name,
-      email: decoded.email
-    })
-  }
-
   render() {
     return (
-             
-       <span> 
-          <span> {this.state.first_name} </span> 
-          <span> { }</span>
-          <span> { this.state.last_name}</span> 
-        </span>
-    )
+      <span>
+        <span> {this.state.first_name} </span>
+        <span> {}</span>
+        <span> {this.state.last_name}</span>
+      </span>
+    );
   }
 }
-export default UserView
+export default UserView;

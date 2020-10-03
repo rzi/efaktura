0f713a227b50c7cfb91d307474506be52ba10aef
rzi <rzi@vp.pl>
2020-10-03 21:49:46 +0200

    commit after -4 comits

--- client/src/App.js
+++ client/src/App.js
@@ -13,65 +13,13 @@ import NewPassword from "./components/NewPassword.js";
 import ChangePassword from "./components/ChangePassword.js";
 import NewPasswordView from "./components/NewPasswordView.js";
 import UserActivated from "./components/UserActivated.js";
-// import UserView from "./components/UserView.js";
 import jwt_decode from "jwt-decode";
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      first_name: "",
-      last_name: "",
-      email: "",
-      errors: {},
-    };
-  }
-  componentDidMount() {
-    var token = localStorage.usertoken;
-    console.log(`DID nav token ${token}`);
-    if (localStorage.usertoken === undefined) {
-      console.log(`DID jestem w if `);
-      // localStorage.setItem("usertoken", "");
-      return;
-    } else {
-      console.log(`DID jestem w else `);
-      const token = localStorage.usertoken;
-      const decoded = jwt_decode(token);
-
-      console.log(`DID decoded2 ${decoded}`);
-      console.log(`DID decoded.first_name ${decoded.first_name}`);
-      this.setState({
-        first_name: decoded.first_name,
-        last_name: decoded.last_name,
-        email: decoded.email,
-      });
-    }
-  }
-  componentWillMount() {
-    var token = localStorage.usertoken;
-    console.log(`WILL nav token ${token}`);
-    if (localStorage.usertoken === undefined) {
-      console.log(`WILL jestem w if `);
-      // localStorage.setItem("usertoken", "");
-      return;
-    } else {
-      console.log(`WILL jestem w else `);
-      const token = localStorage.usertoken;
-      const decoded = jwt_decode(token);
-
-      console.log(`WILL decoded2 ${decoded}`);
-      console.log(`WILL decoded.first_name ${decoded.first_name}`);
-      this.setState({
-        first_name: decoded.first_name,
-        last_name: decoded.last_name,
-        email: decoded.email,
-      });
-    }
-  }
   render() {
     return (
       <Router>
         <div className="App">
-          <Navbar first_name={this.state.first_name} last_name={this.state.last_name  }/>
+          <Navbar />
           <Route exact path="/" component={Landing} />
           <div className="container">
             <Route exact path="/register" component={Register} />
--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,73 +1,122 @@
-import React, { Component } from 'react'
-import jwt_decode from 'jwt-decode'
-import Tabs from 'react-bootstrap/Tabs'
-import Tab from 'react-bootstrap/Tab'
-import {Button} from "react-bootstrap";
+import React, { Component } from "react";
+import jwt_decode from "jwt-decode";
+import Tabs from "react-bootstrap/Tabs";
+import Tab from "react-bootstrap/Tab";
+import { Button } from "react-bootstrap";
+import axios from "axios";
 class Profile extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      first_name: '',
-      last_name: '',
-      email: '',
-      errors: {}
-    }
+      first_name: "",
+      last_name: "",
+      email: "",
+      btnIsDiasebled: true,
+      errors: {},
+      value: "",
+    };
+    this.handleChangeFirst_name = this.handleChangeFirst_name.bind(this);
+    this.handleChangeLast_name = this.handleChangeLast_name.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
-    const token = localStorage.usertoken
-    const decoded = jwt_decode(token)
+    const token = localStorage.usertoken;
+    const decoded = jwt_decode(token);
     this.setState({
       first_name: decoded.first_name,
       last_name: decoded.last_name,
       email: decoded.email,
-      btnIsDiasebled : false,
-    })
+      btnIsDiasebled: true,
+    });
   }
 
-   render(){
+  handleChangeFirst_name(event) {
+    this.setState({ first_name: event.target.value });
+  }
+  handleChangeLast_name(event) {
+    this.setState({ last_name: event.target.value });
+  }
+
+  handleSubmit(event) {
+    axios
+      .put("/users/update", {
+        first_name: this.state.first_name,
+        last_name: this.state.last_name,
+        email: this.state.email,
+      })
+      .then((response) => {
+        console.log("Dane użytkownika uaktualnione");
+        localStorage.removeItem("usertoken");
+        window.location.href = "/login";
+      });
+    event.preventDefault();
+  }
+
+  render() {
     return (
       <div>
-        <Tabs defaultActiveKey="profile" transition={false} id="noanim-tab-example">
-          <Tab eventKey="profile" title="Profil użytkownika">             
-             <div className="col-sm-8 mx-auto">
-                <br/>
-                <h3 className="text-center">Twój profil</h3>
-             </div>
-             <div className="tab-content">
-              <table className="table col-md-6 mx-auto">
-                <tbody>
-                  <tr>
-                        <td>Imię</td>
-                        <td>{this.state.first_name}</td>
-                        <td><Button className="Button" variant="secondary">Edycja</Button></td>
-                        <td>{ this.state.btnIsDiasebled && <Button className="Button" variant="primary" >Zapisz</Button> }</td>
-                      </tr>
-                  <tr>
-                        <td>Nazwisko</td>
-                        <td>{ this.state.btnIsDiasebled && <input type="submit" value={this.state.last_name} onClick={this.onClick} />}</td>
-                        <td> <Button className="Button" variant="secondary">Edycja</Button></td>
-                        <td>{ this.state.btnIsDiasebled && <Button className="Button" variant="primary" >Zapisz</Button> }</td>
-                      </tr>
-                  <tr>
-                        <td>Email</td>
-                        <td>{this.state.email}</td>
-                      </tr>
-                </tbody>
-              </table>
-             </div>  
+        <Tabs
+          defaultActiveKey="profile"
+          transition={false}
+          id="noanim-tab-example"
+        >
+          <Tab eventKey="profile" title="Profil użytkownika">
+            <div className="col-sm-8 mx-auto">
+              <br />
+              <h3 className="text-center">Twój profil</h3>
+            </div>
+            <div className="tab-content">
+              <form onSubmit={this.handleSubmit}>
+                <div className="form-group">
+                  <label>
+                    Email:                  </label>
+                    <input type="text" value={this.state.email} readOnly className="form-control-plaintext" />
+
+                </div>
+                <div className="form-group">
+                  <label>
+                    Imię:
+                    <br />
+                    <input
+                      type="text"
+                      className="form-control"
+                      value={this.state.first_name}
+                      onChange={this.handleChangeFirst_name}
+                    />
+                  </label>
+                </div>
+                <div className="form-group">
+                  <label>
+                    Nazwisko:
+                    <br />
+                    <input
+                      type="text"
+                      className="form-control"
+                      value={this.state.last_name}
+                      onChange={this.handleChangeLast_name}
+                    />
+                  </label>
+                </div>
+                <input
+                  className="btn btn-secondary"
+                  type="submit"
+                  value="Uaktualnij"
+                />
+              </form>
+            </div>
           </Tab>
           <Tab eventKey="invoice" title="Dane do faktur">
-          <br/>
-          <h3 className="text-center">Twoje dane do fakturowania</h3>
+            <br />
+            <h3 className="text-center">Twoje dane do fakturowania</h3>
           </Tab>
-          <Tab eventKey="settings" title="Ustawiena" >
-          <br/>
-          <h3 className="text-center">Twoje ustawienia </h3>
+          <Tab eventKey="settings" title="Ustawiena">
+            <br />
+            <h3 className="text-center">Twoje ustawienia </h3>
           </Tab>
         </Tabs>
       </div>
-     )  
-   }
+    );
+  }
 }
-export default Profile
+export default Profile;
